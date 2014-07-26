9593536eb7c30c939630abbbfe857cef91b8b7ca
Alex Zimin <azimin@me.com>
2014-07-26 14:42:30 +0400

    - New hierarchy

---
+++ SberbankMobileMoneyTracker/SberbankMobileMoneyTracker/CustomViews/CalendarView/CalendarView.h
@@ -0,0 +1,13 @@
+//
+//  CalendarView.h
+//  SberbankMobileMoneyTracker
+//
+//  Created by Alex Zimin on 26/07/14.
+//  Copyright (c) 2014 Empatika. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalendarView : UIView
+
+@end
---
+++ SberbankMobileMoneyTracker/SberbankMobileMoneyTracker/CustomViews/CalendarView/CalendarView.m
@@ -0,0 +1,29 @@
+//
+//  CalendarView.m
+//  SberbankMobileMoneyTracker
+//
+//  Created by Alex Zimin on 26/07/14.
+//  Copyright (c) 2014 Empatika. All rights reserved.
+//
+
+#import "CalendarView.h"
+
+@implementation CalendarView
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
