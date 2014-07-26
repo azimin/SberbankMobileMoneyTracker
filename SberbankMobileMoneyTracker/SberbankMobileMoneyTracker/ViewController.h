7133bec963dc57abf6be3373dc18b1e2367a9ed4
Alex Zimin <azimin@me.com>
2014-07-26 14:17:11 +0400

    - Base

---
+++ SberbankMobileMoneyTracker/SberbankMobileMoneyTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SberbankMobileMoneyTracker
+//
+//  Created by Alex Zimin on 26/07/14.
+//  Copyright (c) 2014 Empatika. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SberbankMobileMoneyTracker/SberbankMobileMoneyTracker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SberbankMobileMoneyTracker
+//
+//  Created by Alex Zimin on 26/07/14.
+//  Copyright (c) 2014 Empatika. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
